4870c93ed90365cfbba6257789bb78476df220d7
Dinesh <dineshbseervi@gmail.com>
2025-07-29 01:55:22 +0530

    add auth-service & security at all services

--- AuthService/src/main/java/com/authh/config/AuthConfig.java
+++ AuthService/src/main/java/com/authh/config/AuthConfig.java
@@ -43,25 +43,6 @@ public class AuthConfig {
     }
 
 
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//
-//        UserDetails user1 = User
-//                .withDefaultPasswordEncoder()
-//                .username("kiran")
-//                .password("k@123")
-//                .roles("USER")
-//                .build();
-//
-//        UserDetails user2 = User
-//                .withDefaultPasswordEncoder()
-//                .username("harsh")
-//                .password("h@123")
-//                .roles("ADMIN")
-//                .build();
-//        return new InMemoryUserDetailsManager(user1, user2);
-//    }
-
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
